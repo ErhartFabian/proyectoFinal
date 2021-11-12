cd8a62e6a4cbe9f59d4fef5d1cba5eb56dacc4b7
ErhartFabian <A01274089@itesm.mx>
2021-11-12 17:18:40 -0600

    cambios en app js (router)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route,Switch,Link,NavLink } from "react-router-dom";
+import {Home,Inventory,PaymentProcess} from './components'
 import { Fragment } from 'react/cjs/react.production.min';
 
 function App() {
   return (
     <Fragment>
-      
+
       <Router>
 
       </Router>
