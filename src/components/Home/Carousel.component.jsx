ec5ea8512e07ce6917668f5ac8902d772b0767b1
ErhartFabian <A01274089@itesm.mx>
2021-11-23 18:15:02 -0600

    Update Carousel.component.jsx

--- src/components/Home/Carousel.component.jsx
+++ src/components/Home/Carousel.component.jsx
@@ -6,14 +6,14 @@ import './home.css';
 export default (props) => (
   <Carousel autoPlay>
       {
-         props.products.map((product) => {
+         /* props.products.map((product) => {
             return (
                 <div className="carousel" >
                 <img alt="" src={product.image} key={product.title} />
                 <p className="legend">{product.title} <br></br> &#36;{product.price}</p>
                 </div>
             )})
-        
+         */
       }
   </Carousel>
 );
